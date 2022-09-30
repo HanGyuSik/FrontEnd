b58426f06c9c47cbd01188f6f8051052da6bf56f
HanGysSik <gkfrbtlr1357@naver.com>
2022-09-30 18:09:09 +0900

    112

--- pj3/scripts/arr.js
+++ pj3/scripts/arr.js
@@ -108,3 +108,18 @@ for (let i = 0; i < num1.length; i++) {
   console.log(num1);
 }
 
+
+for (let i = 0; i < num1.length; i++) {
+  let idx1 = Math.floor(Math.random() * 5); // 0~4까지 랜덤수 생성
+  let idx2 = Math.floor(Math.random() * 5); // 0~4까지 랜덤수 생성
+
+  if (idx1 == idx2) continue;
+
+  //idx1 위치의 값과 idx2위치의 값을 바꿔치기
+  let temp = num1[idx1];
+  num1[idx1] = num1[idx2];
+  num1[idx2] = temp;
+
+  console.log(`idx1 = ${idx1}, idx2 = ${idx2}`);
+  console.log(num1);
+}
--- pj3/scripts/index.js
+++ pj3/scripts/index.js
@@ -17,32 +17,3 @@ let shuffleFlag = false;
 document.addEventListener("DOMContentLoaded", ()=>{
    
 });
-
-
-/* 배열 섞기*/
-let num1 = [1, 2, 3, 4, 5];
-num1.sort();
-console.log(num1);
-
-const shuffleOld = (num1) => {
-  for (let i = 0; i < num1.length; i++) {
-    let idx1 = Math.floor(Math.random) * 5; // 0~4까지 랜덤수 생성
-    let idx2 = Math.floor(Math.random) * 5; // 0~4까지 랜덤수 생성
-
-    if (idx1 == idx2) continue;
-
-    //idx1 위치의 값과 idx2위치의 값을 바꿔치기
-    let temp = num1[idx1];
-    num1[idx1] = num1[idx2];
-    num1[idx2] = temp;
-
-    console.log(`idx1 = ${idx1}, idx2 = ${idx2}`);
-    console.log(num1);
-  }
-}
-
-const shuffle = (array) => {
-  return array.sort(() => Math.random() - 0.5);
-}
-console.log(shuffleOld);
-console.log(num1);
